9c18786c2080c54516d967cec1bd5be5e2ddce51
Xiaohan Chen <xiaohan.chen@utexas.edu>
2021-04-28 08:42:16 -0500

    EB for 90% sparsity

--- main.py
+++ main.py
@@ -378,9 +378,11 @@ best_prec1 = 0.
 flag_30 = True
 flag_50 = True
 flag_70 = True
+flag_90 = True
 early_bird_30 = EarlyBird(0.3)
 early_bird_50 = EarlyBird(0.5)
 early_bird_70 = EarlyBird(0.7)
+early_bird_90 = EarlyBird(0.9)
 for epoch in range(args.start_epoch, args.epochs):
     if early_bird_30.early_bird_emerge(model):
         print("[early_bird_30] Find EB!!!!!!!!!, epoch: "+str(epoch))
@@ -412,6 +414,16 @@ for epoch in range(args.start_epoch, args.epochs):
             'optimizer': optimizer.state_dict(),
             }, is_best, 'EB-70-'+str(epoch+1), filepath=args.save)
             flag_70 = False
+    if early_bird_90.early_bird_emerge(model):
+        print("[early_bird_90] Find EB!!!!!!!!!, epoch: "+str(epoch))
+        if flag_90:
+            save_checkpoint({
+            'epoch': epoch + 1,
+            'state_dict': model.state_dict(),
+            'best_prec1': best_prec1,
+            'optimizer': optimizer.state_dict(),
+            }, is_best, 'EB-90-'+str(epoch+1), filepath=args.save)
+            flag_90 = False
     if epoch in args.schedule:
         for param_group in optimizer.param_groups:
             param_group['lr'] *= 0.1
