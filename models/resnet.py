bb5843300aabdf4aeadc9524625e4d11b93f6465
Xiaohan Chen <xiaohan.chen@utexas.edu>
2021-04-28 10:45:58 -0500

    Compatible with pruning

--- models/resnet.py
+++ models/resnet.py
@@ -12,6 +12,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 import math
 import numpy as np
+from .channel_selection import channel_selection
 
 
 __all__ = ['resnet_cifar']
@@ -20,11 +21,15 @@ __all__ = ['resnet_cifar']
 class BasicBlock(nn.Module):
     expansion = 1
 
-    def __init__(self, in_planes, planes, stride=1):
+    def __init__(self, in_planes, planes, stride=1, cfg=None):
+        assert cfg is not None
         super(BasicBlock, self).__init__()
-        self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
-        self.bn1 = nn.BatchNorm2d(planes)
-        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=1, padding=1, bias=False)
+
+        self.select = channel_selection(in_planes)
+
+        self.conv1 = nn.Conv2d(cfg[0], cfg[1], kernel_size=3, stride=stride, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(cfg[1])
+        self.conv2 = nn.Conv2d(cfg[1], planes, kernel_size=3, stride=1, padding=1, bias=False)
         self.bn2 = nn.BatchNorm2d(planes)
 
         self.shortcut = nn.Sequential()
@@ -35,6 +40,7 @@ class BasicBlock(nn.Module):
             )
 
     def forward(self, x):
+        out = self.select(x)
         out = F.relu(self.bn1(self.conv1(x)))
         out = self.bn2(self.conv2(out))
         out += self.shortcut(x)
@@ -71,16 +77,33 @@ class Bottleneck(nn.Module):
 
 
 class ResNet(nn.Module):
-    def __init__(self, block, num_blocks, num_classes=10, init_method='standard'):
+    def __init__(self, block, num_blocks, num_classes=10, init_method='standard', cfg=None):
         super(ResNet, self).__init__()
         self.in_planes = 64
 
+        if cfg is None:
+            # Construct config variable.
+            cfg = [
+                [64],
+                [64, 64] * num_blocks[0],
+                # [128, 128, 128], [128, 128]*(num_blocks[1]-1),
+                # [256, 256, 256], [256, 256]*(num_blocks[2]-1),
+                # [512, 512, 512], [512, 512]*(num_blocks[3]-1),
+                [128, 128] * num_blocks[1],
+                [256, 256] * num_blocks[2],
+                [512, 512] * num_blocks[3],
+            ]
+            cfg = [item for sub_list in cfg for item in sub_list]
+
         self.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1, bias=False)
         self.bn1 = nn.BatchNorm2d(64)
-        self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1)
-        self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
-        self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
-        self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2)
+        self.select = channel_selection(64 * block.expansion)
+
+        self.layer1 = self._make_layer(block, 64 , num_blocks[0], stride=1, cfg=cfg[0                    :2*sum(num_blocks[:1])])
+        self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2, cfg=cfg[2*sum(num_blocks[:1]):2*sum(num_blocks[:2])])
+        self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2, cfg=cfg[2*sum(num_blocks[:2]):2*sum(num_blocks[:3])])
+        self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2, cfg=cfg[2*sum(num_blocks[:3]):2*sum(num_blocks[:4])])
+
         self.linear = nn.Linear(512*block.expansion, num_classes)
 
         self.reset_conv_parameters(init_method)
@@ -140,56 +163,58 @@ class ResNet(nn.Module):
                 non_bop_params += list(m.parameters())
         return non_bop_params
 
-    def _make_layer(self, block, planes, num_blocks, stride):
+    def _make_layer(self, block, planes, num_blocks, stride, cfg):
         strides = [stride] + [1]*(num_blocks-1)
         layers = []
-        for stride in strides:
-            layers.append(block(self.in_planes, planes, stride))
+        for i, stride in enumerate(strides):
+            layers.append(block(self.in_planes, planes, stride, cfg[2*i:2*(i+1)]))
             self.in_planes = planes * block.expansion
         return nn.Sequential(*layers)
 
     def forward(self, x):
-        out = F.relu(self.bn1(self.conv1(x)))
+        out = F.relu(self.select(self.bn1(self.conv1(x))))
+
         out = self.layer1(out)
         out = self.layer2(out)
         out = self.layer3(out)
         out = self.layer4(out)
+
         out = F.avg_pool2d(out, 4)
         out = out.view(out.size(0), -1)
         out = self.linear(out)
         return out
 
 
-def resnet_cifar(dataset='cifar10', depth=18):
+def resnet_cifar(dataset='cifar10', depth=18, cfg=None):
     assert dataset == 'cifar10'
     if depth == 18:
-        return ResNet18(init_method='kaiming_normal')
+        return ResNet18(init_method='kaiming_normal', cfg=cfg)
     elif depth == 34:
-        return ResNet34(init_method='kaiming_normal')
+        return ResNet34(init_method='kaiming_normal', cfg=cfg)
     elif depth == 50:
-        return ResNet34(init_method='kaiming_normal')
+        return ResNet50(init_method='kaiming_normal', cfg=cfg)
     elif depth == 101:
-        return ResNet34(init_method='kaiming_normal')
+        return ResNet101(init_method='kaiming_normal', cfg=cfg)
     elif depth == 152:
-        return ResNet34(init_method='kaiming_normal')
+        return ResNet152(init_method='kaiming_normal', cfg=cfg)
     else:
         raise ValueError('invalid arch type')
 
 
-def ResNet18(init_method='standard'):
-    return ResNet(BasicBlock, [2,2,2,2], init_method=init_method)
+def ResNet18(init_method='standard', cfg=None):
+    return ResNet(BasicBlock, [2,2,2,2], init_method=init_method, cfg=cfg)
 
-def ResNet34(init_method='standard'):
-    return ResNet(BasicBlock, [3,4,6,3], init_method=init_method)
+def ResNet34(init_method='standard', cfg=None):
+    return ResNet(BasicBlock, [3,4,6,3], init_method=init_method, cfg=cfg)
 
-def ResNet50(init_method='standard'):
-    return ResNet(Bottleneck, [3,4,6,3], init_method=init_method)
+def ResNet50(init_method='standard', cfg=None):
+    return ResNet(Bottleneck, [3,4,6,3], init_method=init_method, cfg=cfg)
 
-def ResNet101(init_method='standard'):
-    return ResNet(Bottleneck, [3,4,23,3], init_method=init_method)
+def ResNet101(init_method='standard', cfg=None):
+    return ResNet(Bottleneck, [3,4,23,3], init_method=init_method, cfg=cfg)
 
-def ResNet152(init_method='standard'):
-    return ResNet(Bottleneck, [3,8,36,3], init_method=init_method)
+def ResNet152(init_method='standard', cfg=None):
+    return ResNet(Bottleneck, [3,8,36,3], init_method=init_method, cfg=cfg)
 
 
 def test():
